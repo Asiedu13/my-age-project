18cad7fb73dbf794a2d3b1e41e57e16f28518b9c
prince <princekofasiedu@gmail.com>
2023-04-24 18:31:43 +0000

    Changed algorithm for year and month calculation

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ const launch = () => {
     // Get elements
     let inputElements = document.querySelectorAll('.input_e')
     let messagePoints = document.querySelectorAll('.message')
-    let shouldCompute = false;
+    let shouldGetDaysLived = false;
     let vals = getInputValues(inputElements);
     let if_null = if_empty(vals);
 
@@ -23,37 +23,35 @@ const launch = () => {
         goodYear = verifyYear(vals[2])
         
        let verifyAll = verifyValues(goodDay, goodMonth, goodYear);
-        shouldCompute = verifyAll;
+        shouldGetDaysLived = verifyAll;
     }
 
-    if (shouldCompute) {
+    if (shouldGetDaysLived) {
         // Find the years, month
-
-        let d1 = new Date(`${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`);
+        let d1 = new Date(`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`);
         let d2 = new Date(`${vals[2]}-${vals[1]}-${vals[0]}`)
-        let yearsLived = (d1.getTime() - d2.getTime()) ;
-        
-        function convertmili( mSeconds ){
-            var checkYear = Math.floor(mSeconds / 31536000000);
-            return checkYear;
-        }
-        yearsLived = convertmili(yearsLived)
+        let yearsLivedInMillisecond = (d1.getTime() - d2.getTime()) ; // returns in mSeconds
+        let differenceInDays = convertmili(yearsLivedInMillisecond);
+
+        let yearsLived = yearsLivedCalc(differenceInDays)
+        console.log(yearsLived);
 
         // Getting months lived
-        let monthsLived = Math.abs((new Date().getMonth() + 1) - vals[1])
-
+    
+        let monthsLived = monthsLivedCalc(vals[1]);
+    
         // Get days lived
         let diffOfDays = Math.abs(new Date().getDate() - vals[0])
         
         let dayslived = 0
         if (new Date().getDate() > vals[0] ) {
-            dayslived = new Date().getDate() - diffOfDays
+            dayslived =  diffOfDays
         }
 
         // Edit html
         const display_elem = document.querySelectorAll('.n')
 
-        display_elem[0].textContent = yearsLived;
+        display_elem[0].textContent = yearsLived.wholeValue;
         display_elem[1].textContent = monthsLived;
         display_elem[2].textContent = dayslived;
     }
@@ -92,80 +90,4 @@ const if_empty = (arr) => {
     }
     return {anyEmpty: any, retarr: retarr}
 
-}
-const verifyValues = (goodDay, goodMonth, goodYear) => {
-   
-    let retVal = false;
-    if (goodDay.length > 0 || goodMonth.length > 0 || goodYear.length > 0) {
-        err_display([goodDay, goodMonth, goodYear])
-    }else {
-        correct_display([goodDay, goodMonth, goodYear])
-
-        retVal = true;
-    }
-
-    return retVal;
-}
-const verifyDay = (dayInput) => {
-    let retVal = ''
-    let day = Number.parseInt(dayInput)
-    if (Number.isInteger(day)) {
-        if (day < 1 || day > 31 ) {
-            retVal = 'Must be a valid day'
-        }
-    } else {
-        retVal = ''
-    }
-
-    return retVal
-}
-
-const verifyMonth = (monthInput) => {
-    let retVal = ''
-    let month = Number.parseInt(monthInput)
-    if (Number.isInteger(month)) {
-        if (month < 1 || month > 12 ) {
-            retVal = 'Must be a valid month'
-        }
-    }else {
-        retVal = ''
-    }
-
-    return retVal
-}
-
-const verifyYear = (yearInput) => {
-    let retVal = ''
-    let year = Number.parseInt(yearInput)
-    if (Number.isInteger(year)) {
-        if (year < 1900 || year > new Date().getFullYear() ) {
-            retVal = 'Must be a valid year'
-        }
-    }else {
-        retVal = ''
-    }
-
-    return retVal
-}
-
-const err_display = (arr) => {
-    const errMessagePoints = document.querySelectorAll('.message');
-    for (let i = 0; i < errMessagePoints.length; i++) {
-        errMessagePoints[i].textContent = arr[i]
-    }
-
-    labels.forEach(label => {
-        label.classList.add('text-error')
-    })
-}
-
-const correct_display = (arr) => {
-    const errMessagePoints = document.querySelectorAll('.message');
-    for (let i = 0; i < errMessagePoints.length; i++) {
-        errMessagePoints[i].textContent = arr[i]
-    }
-
-    labels.forEach(label => {
-        label.classList.remove('text-error')
-    })
 }
---
+++ assets/js/utils.js
@@ -0,0 +1,112 @@
+// ------ DaysLived Computation
+function convertmili( mSeconds ){
+    // var checkYear = Math.floor(mSeconds / 31536000000);
+    var one_day=1000*60*60*24
+    let days = Math.floor(mSeconds / one_day);
+    return days;
+}
+
+
+function modInfo(days, n) {
+    return {wholeValue: Math.floor(days / n), remainder: days % n}
+}
+
+function yearsLivedCalc(days) {
+    return {wholeValue: Math.floor(days / 365), remainder: days % 365}
+}
+
+function monthsLivedCalc(month) {
+    let currentMonth = new Date().getMonth() + 1;
+    let monthsLived = 0;
+    if (currentMonth == month) {
+         monthsLived = 0
+    }else if (currentMonth < month) {
+        monthsLived = currentMonth;
+    }else {
+        monthsLived = currentMonth - month;
+    }
+
+    return monthsLived;
+}
+
+
+
+
+// ---- Value verification functions ---------
+
+const verifyValues = (goodDay, goodMonth, goodYear) => {
+   
+    let retVal = false;
+    if (goodDay.length > 0 || goodMonth.length > 0 || goodYear.length > 0) {
+        err_display([goodDay, goodMonth, goodYear])
+    }else {
+        correct_display([goodDay, goodMonth, goodYear])
+
+        retVal = true;
+    }
+
+    return retVal;
+}
+const verifyDay = (dayInput) => {
+    let retVal = ''
+    let day = Number.parseInt(dayInput)
+    if (Number.isInteger(day)) {
+        if (day < 1 || day > 31 ) {
+            retVal = 'Must be a valid day'
+        }
+    } else {
+        retVal = ''
+    }
+
+    return retVal
+}
+
+const verifyMonth = (monthInput) => {
+    let retVal = ''
+    let month = Number.parseInt(monthInput)
+    if (Number.isInteger(month)) {
+        if (month < 1 || month > 12 ) {
+            retVal = 'Must be a valid month'
+        }
+    }else {
+        retVal = ''
+    }
+
+    return retVal
+}
+
+const verifyYear = (yearInput) => {
+    let retVal = ''
+    let year = Number.parseInt(yearInput)
+    if (Number.isInteger(year)) {
+        if (year < 1900 || year > new Date().getFullYear() ) {
+            retVal = 'Must be a valid year'
+        }
+    }else {
+        retVal = ''
+    }
+
+    return retVal
+}
+
+const err_display = (arr) => {
+    const errMessagePoints = document.querySelectorAll('.message');
+    for (let i = 0; i < errMessagePoints.length; i++) {
+        errMessagePoints[i].textContent = arr[i]
+    }
+
+    labels.forEach(label => {
+        label.classList.add('text-error')
+    })
+}
+
+const correct_display = (arr) => {
+    const errMessagePoints = document.querySelectorAll('.message');
+    for (let i = 0; i < errMessagePoints.length; i++) {
+        errMessagePoints[i].textContent = arr[i]
+    }
+
+    labels.forEach(label => {
+        label.classList.remove('text-error')
+    })
+}
